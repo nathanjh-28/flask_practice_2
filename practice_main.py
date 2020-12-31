e347008d4c02f8c9d8a2162d1b059506df834098
nathanjh-28 <nathan@nathanjharris.com>
2020-12-30 19:42:24 -0700

    added registration form, login form, and respective templates

--- practice_main.py
+++ practice_main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
@@ -18,9 +19,28 @@ posts = [{
 }]
 # ===================================================================
 
+# secret key
+app.config['SECRET_KEY'] = 'SAPNgFaCXp6CZTjq'
+
 @app.route("/")
 def home():
     return render_template('home.html', posts=posts)
+
+@app.route("/")
+def about():
+    return render_template('about.html')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
