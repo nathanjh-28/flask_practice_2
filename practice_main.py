a1bec26c2359b88e36aabfcf36b592bdfb5c0231
nathanjh-28 <nathan@nathanjharris.com>
2020-12-30 17:21:30 -0700

    added dummy data and rendered it on home template

--- practice_main.py
+++ practice_main.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# ===================================================================
+#_________   Dummy Data
+posts = [{
+'author':'Nathan Harris',
+'title':'Blog Post 1',
+'content':'Lorem Ipsum blah blah blah',
+'date_posted':'April 20, 2018'
+},
+{
+'author':'Jeff Smitty',
+'title':'Blog Post 2',
+'content':'hello world my name is Jeffff and I like to party!',
+'date_posted':'May 1, 2018'
+}]
+# ===================================================================
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
